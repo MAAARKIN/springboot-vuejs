46a92ae1f7f82eab731a2ad0f378e458a13a3069
Marcos Filho <sdksdk@sdksdk.local>
2017-02-20 00:32:32 -0300

    first commit

---
+++ app/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>{{greeting}} e livereload!! </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                greeting: 'Funcionou!!!'
+            }
+        }
+    }
+</script>
---
+++ src/main/java/br/com/browserify/SpringbootVuejsApplication.java
@@ -0,0 +1,12 @@
+package br.com.browserify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVuejsApplication.class, args);
+	}
+}
